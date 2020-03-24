5c1acff5a6087d997463fd9b33b833dbcc875940
Alexander <paulo.alexander12@gmail.com>
2020-03-23 20:48:11 -0500

    este funciona
    @Component({
      //selector: '[app-servers]',
      selector: '.app-servers',
       templateUrl: './servers.component.html',
       styleUrls: ['./servers.component.css']
    })

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: '[app-servers]',
+  selector: '.app-servers',
    templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+   styleUrls: ['./servers.component.css'] 
 })
 export class ServersComponent implements OnInit {
 
@@ -11,5 +12,4 @@ export class ServersComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
